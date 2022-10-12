f9305dc25111cff464c7b046e90ff800f3dbec7d
Artur0705 <artlil420@gmail.com>
2022-10-12 18:11:57 +1100

    Trying to fix heroku

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 const path = require("path");
+require("dotenv").config();
 const db = require("./config/connection");
 const { authMiddleware } = require("./utils/auth");
 
